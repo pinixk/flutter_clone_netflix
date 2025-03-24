b646d87fd3c0de940e3672516582cb6b82cf9323
pinixk <bjkim0110@gmail.com>
2025-03-24 17:19:25 +0900

    feat : carousel slider 적용

--- lib/screens/home/_components/carosel_slider.dart
+++ lib/screens/home/_components/carosel_slider.dart
@@ -1,16 +1,17 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_netflix/models/movie.dart';
 
-class CaroselSlider extends StatefulWidget {
+class CaroselImages extends StatefulWidget {
   List<Movie> movies;
 
-  CaroselSlider({super.key, required this.movies});
+  CaroselImages({super.key, required this.movies});
 
   @override
-  State<CaroselSlider> createState() => _CaroselSliderState();
+  State<CaroselImages> createState() => _CaroselImagesState();
 }
 
-class _CaroselSliderState extends State<CaroselSlider> {
+class _CaroselImagesState extends State<CaroselImages> {
   late List<Movie> movies;
   late List<Widget> posters;
   late List<String> keywords;
@@ -31,6 +32,86 @@ class _CaroselSliderState extends State<CaroselSlider> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Container(padding: const EdgeInsets.all(20)),
+        CarouselSlider(
+          items: posters,
+          options: CarouselOptions(
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currnetPage = index;
+                _currnetKeyword = keywords[_currnetPage];
+              });
+            }
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 3),
+          child: Text(_currnetKeyword, style: const TextStyle(fontSize: 11)),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                likes[_currnetPage]
+                    ? IconButton(onPressed: () {}, icon: const Icon(Icons.check))
+                    : IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+                const Text('내가 찜한 콘텐츠', style: TextStyle(fontSize: 11)),
+              ],
+            ),
+            Container(
+                padding: const EdgeInsets.only(right: 10),
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(backgroundColor: Colors.white),
+                  child: const Row(
+                    children: [
+                      Icon(Icons.play_arrow, color: Colors.black),
+                      Padding(padding: EdgeInsets.all(3)),
+                      Text('재생', style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                )),
+            Container(
+              padding: const EdgeInsets.only(right: 10),
+              child: Column(
+                children: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.info)),
+                  const Text('정보', style: TextStyle(fontSize: 11)),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: makeIndicator(likes, _currnetPage),
+          ),
+        )
+      ],
+    );
   }
+}
+
+List<Widget> makeIndicator(List list, int currentPage) {
+  List<Widget> results = [];
+
+  for (var i = 0; i < list.length; i++) {
+    results.add(Container(
+      width: 8,
+      height: 8,
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 2),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: currentPage == i
+            ? const Color.fromRGBO(255, 255, 255, 0.9)
+            : const Color.fromRGBO(255, 255, 255, 0.4),
+      ),
+    ));
+  }
+
+  return results;
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_netflix/models/movie.dart';
+import 'package:flutter_clone_netflix/screens/home/_components/carosel_slider.dart';
 import 'package:flutter_clone_netflix/screens/home/_components/top_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,14 +19,37 @@ class _HomeScreenState extends State<HomeScreen> {
       'poster' : 'assets/images/test_movie_1.png',
       'like' : false,
     }),
+    Movie.fromMap({
+      'title' : '테스트2',
+      'keyword' : '스릴러',
+      'poster' : 'assets/images/test_movie_1.png',
+      'like' : false,
+    }),
+    Movie.fromMap({
+      'title' : '사랑의 불시착',
+      'keyword' : '사랑/로맨스/판타지',
+      'poster' : 'assets/images/test_movie_1.png',
+      'like' : false,
+    }),
+    Movie.fromMap({
+      'title' : '사랑의 불시착',
+      'keyword' : '사랑/로맨스/판타지',
+      'poster' : 'assets/images/test_movie_1.png',
+      'like' : false,
+    }),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: TopBar(),
-      ),
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            CaroselImages(movies: movies),
+            TopBar(),
+          ],
+        )
+      ],
     );
   }
 }
